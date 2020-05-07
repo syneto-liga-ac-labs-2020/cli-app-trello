8aec823b8abb1836c32f29ce7cae1af8e053eca6
Deni <denisagal09@yahoo.com>
2020-05-07 15:02:09 +0300

    Modified main.py and pyproject.toml

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-key=61a82688e26edda8dd53da6cfe388253
-token=4b904a18f0346b5fb44b1818bf0cf5ea9e8d1d3f573f527657ca9635fc791016 
+#key=61a82688e26edda8dd53da6cfe388253
+#token=4b904a18f0346b5fb44b1818bf0cf5ea9e8d1d3f573f527657ca9635fc791016
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("key", help="Your trello api key")
+parser.add_argument("token", help="Your trello api token")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true") # action="count", default=False
+
+args = parser.parse_args()
