8708d03ae056c9012a40b2bdde50d2ed7e5d1d95
Daicu Denisa <daicudenisa@yahoo.com>
2020-05-07 16:07:01 +0300

    updated main.py

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ def get_user_boards(key, token):
 
     return result
 
+def get_user_lists(key,token,board_id):
+    url= f"https://api.trello.com/1/boards/{board_id}/lists?key={key}&token={token}"
+    response = requests.get(url)
+    result = []
+    for lists in response.json():
+        result.append({'id':lists.get('id'),'name':lists.get('name')})
+
+    return result
 
 
 filepath = './auth.txt'
@@ -36,4 +44,6 @@ parser.add_argument("-v", "--verbose", help="increase output verbosity", action=
 
 args = parser.parse_args()
 
-pprint(get_user_boards(key, token))
+boards_list=get_user_boards(key, token)
+#boards_list[0]
+pprint(get_user_lists(key,token,boards_list[0].get('id')))
