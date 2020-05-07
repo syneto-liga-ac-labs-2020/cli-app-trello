b7328a548808e1fd53d40940177cb5648ad513f9
alexandraveres7 <alexandraveres7@gmail.com>
2020-05-07 15:52:10 +0300

    get user board

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import argparse
 import os
+import requests
+from pprint import pprint
 
 def get_auth(filepath):
     with open(filepath,'r') as f:
         contents = f.readlines()
         return contents[0].strip('\n'),contents[1].strip('\n')
     return ("","")
+
+def get_user_boards(key, token):
+    url = f'https://api.trello.com/1/members/me/boards?key={key}&token={token}'
+    response = requests.get(url)
+    result = []
+    for board in response.json():
+        result.append({'id':board.get('id'), 'name':board.get('name')})
+
+    return result
+
+
 
 filepath = './auth.txt'
 
@@ -22,3 +35,5 @@ print(key,token)
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true") # action="count", default=False
 
 args = parser.parse_args()
+
+pprint(get_user_boards(key, token))
